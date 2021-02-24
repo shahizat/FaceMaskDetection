5b49e117dea4da00da6468869aff791a9f4584f3
shahizat <shahizat005@gmail.com>
2021-02-24 21:08:41 +0500

    Add collect_images.py and dataset

---
+++ collect_images.py
@@ -0,0 +1,42 @@
+import cv2
+import inquirer
+
+questions = [
+  inquirer.List('choice',
+                message="What data do you want to collect?",
+                choices=['with_mask', 'without_mask'],
+            ),
+]
+
+answers = inquirer.prompt(questions)
+name = answers['choice'] 
+
+cam = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! appsink")
+
+cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("press space to take a photo", 500, 300)
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("press space to take a photo", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
